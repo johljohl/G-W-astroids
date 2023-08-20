c4873d63ddbffbe5ec31fe34a452a5e5e1ed5431
johljohl <joohan.lindell@gmail.com>
2023-08-20 10:21:11 +0200

    Updated asteroids

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let numAsteroids = 3;
 let bullets = [];
 let score = 0;
 let lives = 3;
-const shipSize = 20; // Define the ship size here
+const shipSize = 20;
 
 class Asteroid {
   constructor(x, y, size, speedX, speedY) {
@@ -19,11 +19,30 @@ class Asteroid {
     this.size = size;
     this.speedX = speedX;
     this.speedY = speedY;
+    this.vertices = this.generateVertices(); // Lägg till detta
+  }
+
+  // Ny funktion för att generera slumpmässiga vertexpunkter för asteroiden
+  generateVertices() {
+    const numVertices = 5 + Math.floor(Math.random() * 5); // Mellan 5 och 10 vertexpunkter
+    const vertices = [];
+    for (let i = 0; i < numVertices; i++) {
+      const angle = i * (360 / numVertices) * (Math.PI / 180);
+      const distance =
+        this.size + (Math.random() * this.size * 0.4 - this.size * 0.2); // Vissa variationer i avståndet
+      const x = this.x + distance * Math.cos(angle);
+      const y = this.y + distance * Math.sin(angle);
+      vertices.push({ x, y });
+    }
+    return vertices;
   }
 
   draw() {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.moveTo(this.vertices[0].x, this.vertices[0].y);
+    for (let i = 1; i < this.vertices.length; i++) {
+      ctx.lineTo(this.vertices[i].x, this.vertices[i].y);
+    }
     ctx.closePath();
     ctx.strokeStyle = "#666";
     ctx.fillStyle = "#666";
@@ -35,13 +54,19 @@ class Asteroid {
     this.x += this.speedX;
     this.y += this.speedY;
 
-    // If the asteroid goes off the canvas, reset its position and change its direction
+    // Uppdatera vertexpunkterna när asteroiden rör sig
+    for (let vertex of this.vertices) {
+      vertex.x += this.speedX;
+      vertex.y += this.speedY;
+    }
+
     if (this.y > canvas.height + this.size) {
       this.y = -this.size;
       this.x =
         Math.floor(Math.random() * 3) * (canvas.width / 3) + canvas.width / 6;
-      this.speedX = Math.random() * 2 - 1; // Slumpa en hastighet i x-led mellan -1 och 1
-      this.speedY = Math.random() * 2 + 1; // Slumpa en hastighet i y-led mellan 1 och 3
+      this.speedX = Math.random() * 2 - 1;
+      this.speedY = Math.random() * 2 + 1;
+      this.vertices = this.generateVertices(); // Generera nya vertexpunkter när asteroiden återställs
     }
   }
 
@@ -54,11 +79,6 @@ class Asteroid {
         new Asteroid(this.x, this.y, this.size / 2, this.speedX, this.speedY)
       );
     }
-  }
-
-  changeDirection(newSpeedX, newSpeedY) {
-    this.speedX = newSpeedX;
-    this.speedY = newSpeedY;
   }
 }
 
@@ -86,7 +106,6 @@ class Ship {
     this.x += this.speedX;
     this.y += this.speedY;
 
-    // Keep the ship within the canvas boundaries
     if (this.x < 10) {
       this.x = 10;
     } else if (this.x > canvas.width - 10) {
@@ -104,6 +123,7 @@ class Ship {
     bullets.push(new Bullet(this.x, this.y - 20));
   }
 }
+
 class Bullet {
   constructor(x, y) {
     this.x = x;
@@ -114,7 +134,6 @@ class Bullet {
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
-
     ctx.closePath();
     ctx.strokeStyle = "#666";
     ctx.fillStyle = "#666";
@@ -125,7 +144,6 @@ class Bullet {
   update() {
     this.y -= this.speed;
 
-    // Remove the bullet if it goes off the canvas
     if (this.y < -10) {
       bullets.shift();
     }
@@ -140,8 +158,8 @@ function initAsteroids() {
       Math.floor(Math.random() * 3) * (canvas.width / 3) + canvas.width / 6;
     let y = Math.random() * canvas.height;
     let size = 10;
-    let speedX = Math.random() * 2 - 1; // Slumpa en hastighet i x-led mellan -1 och 1
-    let speedY = Math.random() * 2 + 1; // Slumpa en hastighet i y-led mellan 1 och 3
+    let speedX = Math.random() * 2 - 1;
+    let speedY = Math.random() * 2 + 1;
     asteroids.push(new Asteroid(x, y, size, speedX, speedY));
   }
 }
@@ -160,8 +178,7 @@ function handleKeyDown(event) {
     ship.shoot();
   }
 }
-document.addEventListener("keydown", handleKeyDown);
-document.addEventListener("keyup", handleKeyUp);
+
 function handleKeyUp(event) {
   if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
     ship.speedX = 0;
@@ -170,75 +187,7 @@ function handleKeyUp(event) {
   }
 }
 
-btn1.addEventListener("click", function () {
-  ship.shoot();
-});
-
-btn1.addEventListener("touchstart", function () {
-  ship.shoot();
-});
-
-btn2.addEventListener("mouseup", function () {
-  ship.speedX = 0;
-  ship.speedY = 0;
-});
-
-// Lägg till händelsehanterare för touchmove och touchend
-btn2.addEventListener("touchstart", function (event) {
-  // Testa om preventDefault() kan kallas på touchhändelsen
-  if (event.cancelable) {
-    event.preventDefault();
-  }
-
-  if (event.target === btn2) {
-    const touch = event.touches[0];
-    const rect = btn2.getBoundingClientRect();
-    if (touch.clientX < rect.left + rect.width / 2) {
-      // Flytta åt vänster
-      ship.speedX = -3;
-    } else {
-      // Flytta åt höger
-      ship.speedX = 3;
-    }
-
-    if (touch.clientY < rect.top + rect.height / 2) {
-      // Flytta uppåt
-      ship.speedY = -3;
-    } else {
-      // Flytta nedåt
-      ship.speedY = 3;
-    }
-  }
-});
-
-btn2.addEventListener("touchmove", function (event) {
-  if (event.target === btn2) {
-    const touch = event.touches[0];
-    const rect = btn2.getBoundingClientRect();
-    if (touch.clientX < rect.left + rect.width / 2) {
-      // Flytta åt vänster
-      ship.speedX = -3;
-    } else {
-      // Flytta åt höger
-      ship.speedX = 3;
-    }
-
-    if (touch.clientY < rect.top + rect.height / 2) {
-      // Flytta uppåt
-      ship.speedY = -3;
-    } else {
-      // Flytta nedåt
-      ship.speedY = 3;
-    }
-  }
-});
-btn2.addEventListener("touchend", function () {
-  ship.speedX = 0;
-  ship.speedY = 0;
-});
-
 function checkCollisions() {
-  // Check collisions between bullets and asteroids
   for (let i = 0; i < bullets.length; i++) {
     for (let j = 0; j < asteroids.length; j++) {
       let dx = bullets[i].x - asteroids[j].x;
@@ -249,20 +198,19 @@ function checkCollisions() {
         bullets.splice(i, 1);
         asteroids[j].split();
         asteroids.splice(j, 1);
-        score += 10; // Increase score when an asteroid is destroyed
+        score += 10;
         break;
       }
     }
   }
 
-  // Check collisions between ship and asteroids
   for (let j = 0; j < asteroids.length; j++) {
     let dx = ship.x - asteroids[j].x;
     let dy = ship.y - asteroids[j].y;
     let distance = Math.sqrt(dx * dx + dy * dy);
 
     if (distance < shipSize + asteroids[j].size) {
-      lives--; // Decrease lives when ship collides with an asteroid
+      lives--;
       asteroids.splice(j, 1);
       break;
     }
@@ -287,28 +235,86 @@ function update() {
 
   checkCollisions();
 
-  // Draw score and lives on the canvas
-  ctx.font = "16px DS-Digital, sans-serif";
+  ctx.font = "16px DS-Digital"; // Ändrat från "DS-Digital" till "Arial"
   ctx.fillStyle = "black";
   ctx.fillText(`Lives: ${lives}`, 10, 20);
   ctx.fillText(`Score: ${score}`, canvas.width - 80, 20);
 
   if (lives <= 0) {
-    // Stop the game if lives are zero
-    ctx.font = "40px DS-Digital, sans-serif";
+    ctx.font = "40px DS-Digital"; // Ändrat från "DS-Digital" till "Arial"
     ctx.fillStyle = "black";
     ctx.fillText("Game Over", canvas.width / 2 - 86, canvas.height / 2);
     return;
   }
 
   if (asteroids.length === 0) {
-    // Om det inte finns några asteroider, skapa nya och fortsätt spelet
     numAsteroids++;
     initAsteroids();
   }
 
   requestAnimationFrame(update);
 }
+
+document.addEventListener("keydown", handleKeyDown);
+document.addEventListener("keyup", handleKeyUp);
+
+btn1.addEventListener("click", function () {
+  ship.shoot();
+});
+
+btn1.addEventListener("touchstart", function () {
+  ship.shoot();
+});
+
+btn2.addEventListener("mouseup", function () {
+  ship.speedX = 0;
+  ship.speedY = 0;
+});
+
+btn2.addEventListener("touchstart", function (event) {
+  if (event.cancelable) {
+    event.preventDefault();
+  }
+
+  if (event.target === btn2) {
+    const touch = event.touches[0];
+    const rect = btn2.getBoundingClientRect();
+    if (touch.clientX < rect.left + rect.width / 2) {
+      ship.speedX = -3;
+    } else {
+      ship.speedX = 3;
+    }
+
+    if (touch.clientY < rect.top + rect.height / 2) {
+      ship.speedY = -3;
+    } else {
+      ship.speedY = 3;
+    }
+  }
+});
+
+btn2.addEventListener("touchmove", function (event) {
+  if (event.target === btn2) {
+    const touch = event.touches[0];
+    const rect = btn2.getBoundingClientRect();
+    if (touch.clientX < rect.left + rect.width / 2) {
+      ship.speedX = -3;
+    } else {
+      ship.speedX = 3;
+    }
+
+    if (touch.clientY < rect.top + rect.height / 2) {
+      ship.speedY = -3;
+    } else {
+      ship.speedY = 3;
+    }
+  }
+});
+
+btn2.addEventListener("touchend", function () {
+  ship.speedX = 0;
+  ship.speedY = 0;
+});
 
 restartBtn.addEventListener("click", function () {
   location.reload();
